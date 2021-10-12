26b50c62f37b92e98309d861b52f8992e0e312e2
John Woodell <woodie@stageglass.com>
2021-10-12 14:37:40 -0700

    Clean up and lint the config file.

--- config.rb
+++ config.rb
@@ -1,30 +1,28 @@
-=begin
-This file can be used to (e.g.):
-- alter certain inner parts of Gollum,
-- extend it with your stuff.
-
-It is especially useful for customizing supported formats/markups. For more information and
-examples:
-- https://github.com/gollum/gollum#config-file
-
-:authorized_users => ["*@netpress.com"],
-
-=end
+# This file can be used to (e.g.):
+# - alter certain inner parts of Gollum,
+# - extend it with your stuff.
+#
+# It is especially useful for customizing supported formats/markups. For more information and
+# examples:
+# - https://github.com/gollum/gollum#config-file
+#
+# :authorized_users => ["*@netpress.com"],
+#
 
 # Define the wiki options
 wiki_options = {
-  :universal_toc => true,
-  :h1_title => true,
-  :user_icons => 'gravatar',
-  :live_preview => false,
-  :allow_uploads => true,
-  :per_page_uploads => true,
-  :allow_editing => true,
-  :css => false,
-  :js => false,
-  :mathjax => true,
-  :emoji => true,
-  :show_all => true
+  universal_toc: true,
+  h1_title: true,
+  user_icons: "gravatar",
+  live_preview: false,
+  allow_uploads: true,
+  per_page_uploads: true,
+  allow_editing: true,
+  css: false,
+  js: false,
+  mathjax: true,
+  emoji: true,
+  show_all: true
 }
 
 # Send the wiki options to the Gollum app
@@ -32,17 +30,10 @@ Precious::App.set(:wiki_options, wiki_options)
 
 module OverrideMyPrecious
   def commit_message
-    email = request.get_header('HTTP_X_EMAIL') || request.env['X-Email'] || 'nobody@netpress.com'
-    name = email.split('@').first
-    msg = (params[:message].nil? or params[:message].empty?) ? "[no message]" : params[:message]
-
-    commit_message = {
-      message: msg,
-      name: name,
-      email: email
-    }
-
-    commit_message
+    email = request.get_header("HTTP_X_EMAIL") || request.env["X-Email"] || "nobody@netpress.com"
+    name = email.split("@").first
+    msg = params[:message].nil? || params[:message].empty? ? "[no message]" : params[:message]
+    {message: msg, name: name, email: email}
   end
 
   Precious::App.prepend self
